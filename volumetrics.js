940efb86b00b1d0559c0b4c6ea8b56e36a4812c7
Casey Manning <kcgrind@gmail.com>
2022-03-06 15:35:52 -0800

    high density bug fix

--- volumetrics.js
+++ volumetrics.js
@@ -67,6 +67,7 @@ function main() {
     let u_ambient = gl.getUniformLocation(pid, 'u_ambient');
     let u_occlusion = gl.getUniformLocation(pid, 'u_occlusion');
     let u_quality = gl.getUniformLocation(pid, 'u_quality');
+    let u_jitter = gl.getUniformLocation(pid, 'u_jitter');
     
     var SIZE = 32;
     
@@ -125,11 +126,12 @@ function main() {
       gl.uniform2f(mouse, 0.5 - mouseX / w, 0.5 - mouseY / h);
       gl.uniform1f(u_time, t/1000);
       gl.uniform1f(u_density, document.getElementById("densityslider").value);
-      gl.uniform1f(u_shadowfactor, document.getElementById("shadowslider").value);
+      gl.uniform1f(u_shadowfactor, 8);
       gl.uniform1f(u_light, document.getElementById("lightslider").value/ 100.0) ;
       gl.uniform1f(u_ambient, document.getElementById("ambientslider").value/ 100.0) ;
       gl.uniform1f(u_occlusion, document.getElementById("occlusionslider").value) ;
       gl.uniform1i(u_quality, document.getElementById("qualityslider").value) ;
+      gl.uniform1f(u_jitter, document.getElementById("jitterslider").value / 100.0) ;
       gl.viewport(0, 0, w, h);
       gl.clearColor(0, 0, 0, 1);
       gl.drawArrays(gl.TRIANGLES, 0, 3);
